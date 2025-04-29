17c0bdc85e62b17097b9897fa2638329bfb5baa5
El-AouinaYassine <yassine.elaouina1@usmba.ac.ma>
2025-04-29 16:18:37 +0100

    done

---
+++ my-api-project/app/Http/Controllers/CrudController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User; // Import the User model
+
+class CrudController extends Controller
+{
+    public function getAllUsers(){
+        $allUser = User::all();
+        return response()->json(data: ['users'=>$allUser], status: 200);
+    }
+    public function getUserById($id){
+        $user = User::find($id);
+        if($user == null){
+            return response()->json(data:['msg'=>'user not found'],status:404);
+        }else{
+            return response()->json(data:['user'=>$user],status:200);
+        }
+    }
+    public function deleteUserById($id){
+        $user = User::find($id);
+        if($user == null){
+            return response()->json(data:['msg'=>'user not found'],status:404);
+        }else{
+            $user->delete();
+            return response()->json(data:['msg'=>"user $user->name is deleted !!"],status:200);
+        }
+    }
+    public function updateUserById(Request $request ,$id){
+        $user = User::find($id);
+        if($user == null){
+            return response()->json(data:['msg'=>'user not found'],status:404);
+        }else{
+            $user->delete();
+            return response()->json(data:['msg'=>"user $user->name is deleted !!"],status:200);
+        }
+    }
+}
--- my-api-project/routes/api.php
+++ my-api-project/routes/api.php
@@ -1,11 +1,18 @@
 <?php
 
    use App\Http\Controllers\AuthController;
+   use App\Http\Controllers\CrudController;
+   use App\Models\User;
    use Illuminate\Http\Request;
    use Illuminate\Support\Facades\Route;
 
+
    Route::post('/register', [AuthController::class, 'register']);
    Route::post('/login', [AuthController::class, 'login']);
+   Route::get('/users', [CrudController::class, 'getAllUsers'])->name('users.show');
+   Route::get('/users/{userId}', [CrudController::class, 'getUserById'])->name('users.showById');
+   Route::delete('/users/{userId}', [CrudController::class, 'deleteUserById'])->name('users.deleteById');
+   Route::put('/users/{userId}', [CrudController::class, 'updateUserById'])->name('users.updateById');
 
    Route::middleware('auth:api')->group(function () {
        Route::get('/user', function (Request $request) {
